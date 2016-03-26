cf00fcc3cdcbf47f8cfce2688758f9bcf8b2a205
oseifrimpong <obedosei@gmail.com>
2016-03-27 03:09:28 +0800

    Uncomplete devise User view

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  #devise_for :users
+  devise_for :users
 #main homepage route
 root 'homepage#index'
 	
 
 	#routes for users
-	resources :users, :only => [:show]
+	#resources :users, :only => [:show]
 
 end
