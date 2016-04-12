e46e6ecfc3e12443abcf788b03eb45106c6f23d1
Cyril Junior <cyrilselasi@ymail.com>
2016-04-13 01:17:24 +0800

    Include Image Resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160322111832) do
-
+ActiveRecord::Schema.define(version: 20160322123004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +27,6 @@ ActiveRecord::Schema.define(version: 20160322111832) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
@@ -39,7 +36,6 @@ ActiveRecord::Schema.define(version: 20160322111832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
